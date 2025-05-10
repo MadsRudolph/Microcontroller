39a3f598a950d2b00013575dde3c20fa3d76b767
Mads <Mads28122001@hotmail.dk>
2025-05-10 19:52:06 +0200

    Added error handling for MAX/MIN

--- src/main.c
+++ src/main.c
@@ -120,14 +120,25 @@ void process_uart_command(void) {
 
     // Check for MIN or MAX command and store value temporarily
     if (strncmp((char*)uart_buffer, "MIN:", 4) == 0) {
-        temp_min_pwm = atoi((char*)&uart_buffer[4]);
-        uart_send_string("Temp MIN stored. Press button to apply.\r\n");
-        new_pwm_values_received = 1;
+        uint8_t value = atoi((char*)&uart_buffer[4]);
+        if (value >= temp_max_pwm) {
+            uart_send_string("Error: MIN value cannot be greater than MAX!\r\n");
+        } else {
+            temp_min_pwm = value;
+            uart_send_string("Temp MIN stored. Press button to apply.\r\n");
+            new_pwm_values_received = 1;
+        }
     } 
+    
     else if (strncmp((char*)uart_buffer, "MAX:", 4) == 0) {
-        temp_max_pwm = atoi((char*)&uart_buffer[4]);
-        uart_send_string("Temp MAX stored. Press button to apply.\r\n");
-        new_pwm_values_received = 1;
+        uint8_t value = atoi((char*)&uart_buffer[4]);
+        if  (value <= temp_min_pwm) {
+            uart_send_string("Error: MAX value cannot be less than MIN!\r\n");
+        } else {
+            temp_max_pwm = value;
+            uart_send_string("Temp MAX stored. Press button to apply.\r\n");
+            new_pwm_values_received = 1;
+        }
     } 
     else {
         uart_send_string("Invalid UART command! Use MIN: or MAX:\r\n");
